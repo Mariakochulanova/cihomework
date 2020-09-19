6a84a8cc832cbcc8d3402716feebb9f6b542c4c0
Maria Kochulanova <holamarymar@gmail.com>
2020-09-19 22:12:50 +0300

    still needs to be fixed

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -11,10 +11,19 @@ class StatisticsServiceTest {
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
-
         long actual = service.findMax(incomesInBillions);
 
         assertEquals(expected, actual);
     }
 
+    @Test
+    void testFindMax() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+
+    }
 }
